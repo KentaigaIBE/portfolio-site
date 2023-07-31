17b88d6aac4ac875b5dd15a1b673a698874be832
Kentaiga <saigabox@gmail.com>
2023-07-30 17:16:58 -0700

    Swap source order to test mp4 loading first

--- app/art/dua-lipa-spotify/page.tsx
+++ app/art/dua-lipa-spotify/page.tsx
@@ -57,12 +57,12 @@ export default function DuaLipaSpotify() {
           >
             <MediaOutlet>
               <source
-                src="https://kentaiga-portfolio-images.s3.us-east-2.amazonaws.com/LevitatingLoop.mp4"
-                type="video/mp4"
-              />
-              <source
                 src="https://kentaiga-portfolio-images.s3.us-east-2.amazonaws.com/LevitatingLoop.webm"
                 type="video/webm"
+              />
+              <source
+                src="https://kentaiga-portfolio-images.s3.us-east-2.amazonaws.com/LevitatingLoop.mp4"
+                type="video/mp4"
               />
             </MediaOutlet>
           </MediaPlayer>
