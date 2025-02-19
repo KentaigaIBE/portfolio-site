97d7a411955ff378554fe080c4afa43a9a1b6d3f
Kentaiga <saigabox@gmail.com>
2025-02-18 23:08:07 -0800

    Speed up selection transitions

--- components/common/project-preview.tsx
+++ components/common/project-preview.tsx
@@ -2,19 +2,19 @@ import Link from "next/link";
 import Image from "next/image";
 
 export default function ProjectPreview(props: any) {
-  const overlayClass = `${props.color} absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 h-full w-full z-10 opacity-50 group-hover:opacity-0 transition-opacity duration-1000`;
-  const textClass = `${props.textSize} + ${props.textColor} + w-full p-4 text-center transition-all duration-1000 font-black absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 uppercase group-hover:rotate-0 drop-shadow-lg z-20`;
+  const overlayClass = `${props.color} absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 h-full w-full z-10 opacity-50 group-hover:opacity-0 transition-opacity duration-500`;
+  const textClass = `${props.textSize} + ${props.textColor} + w-full p-4 text-center transition-all duration-500 font-black absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 uppercase group-hover:rotate-0 drop-shadow-lg z-20`;
 
   let imageClass =
-    "w-full h-full opacity-50 group-hover:opacity-100 transition-all duration-1000 z-0 object-cover";
+    "w-full h-full opacity-50 group-hover:opacity-100 transition-all duration-500 z-0 object-cover";
   if (props.blurOnHover) {
     imageClass += " blur-0 group-hover:blur-sm";
   }
 
   return (
-    <div className="block justify-start h-[33vh] md:h-96 md:hover:h-[36rem] border-black border-r-8 border-l-8 transition-all duration-1000 relative group border-b-8">
+    <div className="block justify-start h-[33vh] md:h-96 md:hover:h-[36rem] border-black border-r-8 border-l-8 transition-all duration-500 relative group border-b-8">
       <Link href={props.link} className="relative h-full">
-        <div className="relative w-screen h-full group-hover:h-full transition-all duration-1000">
+        <div className="relative w-screen h-full group-hover:h-full transition-all duration-500">
           <Image
             className={imageClass}
             src={props.image}
--- components/home/category.tsx
+++ components/home/category.tsx
@@ -2,13 +2,13 @@ import Link from "next/link";
 import Image from "next/image";
 
 export default function Category(props: any) {
-  const overlayClass = `${props.color} absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 h-full w-full z-10 opacity-50 md:group-hover:opacity-0 transition-opacity duration-1000`;
+  const overlayClass = `${props.color} absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 h-full w-full z-10 opacity-50 md:group-hover:opacity-0 transition-opacity duration-500`;
   return (
-    <div className="flex flex-col w-full md:w-1/4 h-[25vh] md:h-screen border-black border-b-4 border-t-4 md:border-b-0 md:border-t-0 md:border-r-8 md:border-l-8 md:hover:w-full transition-all duration-1000 relative group">
+    <div className="flex flex-col w-full md:w-1/4 h-[25vh] md:h-screen border-black border-b-4 border-t-4 md:border-b-0 md:border-t-0 md:border-r-8 md:border-l-8 md:hover:w-full transition-all duration-500 relative group">
       <Link href={props.link}>
         <div className="relative">
           <Image
-            className="w-full h-[24.3vh] md:h-screen opacity-50 md:group-hover:opacity-100 transition-opacity duration-1000 z-0 object-cover"
+            className="w-full h-[24.3vh] md:h-screen opacity-50 md:group-hover:opacity-100 transition-opacity duration-500 z-0 object-cover"
             src={props.image}
             alt={""}
             width={1920}
@@ -16,7 +16,7 @@ export default function Category(props: any) {
             priority
           />
           <div className={overlayClass}></div>
-          <h2 className="text-orange-300 text-[70px] md:text-[125px] md:group-hover:text-[175px] transition-all duration-1000 font-black absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 uppercase md:-rotate-90 md:group-hover:rotate-0 drop-shadow-lg z-20">
+          <h2 className="text-orange-300 text-[70px] md:text-[125px] md:group-hover:text-[175px] transition-all duration-500 font-black absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 uppercase md:-rotate-90 md:group-hover:rotate-0 drop-shadow-lg z-20">
             {props.name}
           </h2>
         </div>
